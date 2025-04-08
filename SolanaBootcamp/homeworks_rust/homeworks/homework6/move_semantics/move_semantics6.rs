aaffb53b5eb431b19099de863b81aad994910d63
skroflin <sven.kroflin@gmail.com>
2025-04-08 17:33:04 +0200

    move_semantics fixed

--- SolanaBootcamp/homeworks_rust/homeworks/homework6/move_semantics/move_semantics6.rs
+++ SolanaBootcamp/homeworks_rust/homeworks/homework6/move_semantics/move_semantics6.rs
@@ -2,14 +2,14 @@
 // Make me compile! `rustlings hint move_semantics6` for hints
 // You can't change anything except adding or removing references
 
-// I AM NOT DONE
-
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(data.clone());
 
-    string_uppercase(&data);
+    // string_uppercase(&data);
+    // Calling the function and its parameter
+    string_uppercase(data);
 }
 
 // Should not take ownership
@@ -18,8 +18,15 @@ fn get_char(data: String) -> char {
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+// fn string_uppercase(mut data: &String) {
+//     data = data;
+
+//     println!("{}", data);
+// }
+// One way I hanled the error and fix!
+
+fn string_uppercase(mut data: String) {
+    data = data.to_uppercase();
 
     println!("{}", data);
 }
